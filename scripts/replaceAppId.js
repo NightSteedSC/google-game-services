aadc0b89d5aefed642ad59e8d4b2fd18e7e8f3f3
Bartosz Jazdzewski <ibaartek@gmail.com>
2024-05-14 12:14:38 +0200

    u

--- scripts/replaceAppId.js
+++ scripts/replaceAppId.js
@@ -3,19 +3,32 @@ const path = require('path');
 
 module.exports = function(context) {
     const stringsPath = path.join(context.opts.projectRoot, 'platforms/android/app/src/main/res/values/strings.xml');
+    // const newValue = context.opts.plugin.pluginInfo.getPreferences().GOOGLE_GAMES_PROJECT_ID || 'default-value';
+    const newValue = '1056543107496';
+
     fs.readFile(stringsPath, 'utf8', function(err, data) {
         if (err) {
-            throw new Error('Unable to find strings.xml: ' + err);
+            console.error('Error reading strings.xml:', err);
+            return;
         }
 
-        var newValue = context.opts.plugin.pluginInfo.getPreferences().GOOGLE_GAMES_SERVICES_WEB_CLIENT_ID;
-        if (newValue) {
-            var result = data.replace(/<string name="google_games_app_id">[^<]*<\/string>/, `<string name="google_games_app_id">${newValue}</string>`);
-            fs.writeFile(stringsPath, result, 'utf8', function(err) {
-                if (err) {
-                    throw new Error('Failed to write strings.xml: ' + err);
-                }
-            });
+        const regex = /<string name="google_games_app_id">[^<]*<\/string>/;
+        let result;
+
+        if (regex.test(data)) {
+            // Replace existing entry
+            result = data.replace(regex, `<string name="google_games_app_id">${newValue}</string>`);
+        } else {
+            // Append new entry within the <resources> tag
+            result = data.replace(/<\/resources>/, `    <string name="google_games_app_id">${newValue}</string>\n</resources>`);
         }
+
+        fs.writeFile(stringsPath, result, 'utf8', function(writeErr) {
+            if (writeErr) {
+                console.error('Error writing strings.xml:', writeErr);
+            } else {
+                console.log("strings.xml successfully updated.");
+            }
+        });
     });
 };
